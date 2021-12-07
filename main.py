f2a1fa8a5cc6f0bd22823a6db5231664be94e14a
Bibirici Alexandru <alexandru.bibirici@yahoo.com>
2021-12-07 12:23:34 +0200

    change

--- main.py
+++ main.py
@@ -12,24 +12,27 @@ class TestClass():
 
 
     def test_add_user(self):
-        u = Users('asd', 'asdafaddAAA33d@yahoo.com', 'male', 'active')
-        u.add()
+        u = Users('asd1', 'asdafadsdAAA33d@yahoo.com', 'male', 'active')
+        assert u.add()
 
     def test_get_user(self):
-        u = Users('asd', 'asdafaddAA1@yahoo.com', 'male', 'active')
+        u = Users('asd2', 'asdafadsdAA1@yahoo.com', 'male', 'active')
         u.add()
-        u.get()
+        assert u.get()
 
     def test_delete_user(self):
-        u = Users('asd', 'asdafaddAAs1@yahoo.com', 'male', 'active')
+        u = Users('asd3', 'asdafaddsAAs1@yahoo.com', 'male', 'active')
         u.add()
-        u.delete()
+        assert u.delete()
 
     def test_toml1(self):
         TOML.toml_input(TOML.posts_file)
 
     def test_toml2(self):
         TOML.toml_input(TOML.todos_file)
+
+    def test_cleanup(self):
+        assert Object.cleanup()
 
 
 
